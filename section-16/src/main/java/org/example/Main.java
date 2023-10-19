41ef9c18eb766f96b463a06aa9d423631caa2138
Rian Dziuba <riandziuba@gmail.com>
2023-10-19 11:18:16 -0300

    feat: feat: implemented custom exceptions

--- section-16/src/main/java/org/example/Main.java
+++ section-16/src/main/java/org/example/Main.java
@@ -3,10 +3,16 @@ package org.example;
 import org.example.model.entities.UI;
 import org.example.model.entities.boardgame.Position;
 import org.example.model.entities.chess.ChessMatch;
+import org.example.model.exceptions.BoardException;
 
 public class Main {
     public static void main(String[] args) {
-        ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+//        try {
+            ChessMatch chessMatch = new ChessMatch();
+            UI.printBoard(chessMatch.getPieces());
+//        } catch (BoardException e) {
+//            System.out.println("Exception: " + e.getMessage());
+//        }
+
     }
 }
