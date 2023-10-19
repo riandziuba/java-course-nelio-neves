458dced3aa1cdaeece0ae35854a35b20072f5803
Rian Dziuba <riandziuba@gmail.com>
2023-10-19 09:32:50 -0300

    feat: create position class

---
+++ section-16/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import model.entities.boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position position = new Position(1,2);
+        System.out.println(position);
+    }
+}
