5dd716c88e0ab8489d0ade0ac16663163a26e9c4
Rian Dziuba <riandziuba@gmail.com>
2023-11-03 16:05:15 -0300

    King moves

--- section-16/src/main/java/org/example/model/entities/chess/pieces/King.java
+++ section-16/src/main/java/org/example/model/entities/chess/pieces/King.java
@@ -1,12 +1,18 @@
 package org.example.model.entities.chess.pieces;
 
 import org.example.model.entities.boardgame.Board;
+import org.example.model.entities.boardgame.Position;
 import org.example.model.entities.chess.ChessPiece;
 import org.example.model.entities.chess.Color;
 
 public class King extends ChessPiece {
     public King(Board board, Color color) {
         super(board, color);
+    }
+
+    private boolean canMove(Position position) {
+        ChessPiece piece = (ChessPiece) this.getBoard().piece(position);
+        return piece == null || piece.getColor() != this.getColor();
     }
 
     @Override
@@ -17,6 +23,47 @@ public class King extends ChessPiece {
     @Override
     public boolean[][] possibleMoves() {
         boolean[][] moves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position position = new Position(0, 0);
+
+        position.setValues(this.position.getRow() - 1, this.position.getColumn());
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow() + 1, this.position.getColumn());
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow(), this.position.getColumn() - 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow(), this.position.getColumn() + 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
+
+        position.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+        if (this.getBoard().positionExists(position) && this.canMove(position)) {
+            moves[position.getRow()][position.getColumn()] = true;
+        }
         return moves;
     }
 }
