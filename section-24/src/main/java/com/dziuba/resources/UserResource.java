88b9805cd55bdde6c8bd7960d891d86716954485
riandziuba <riandziuba@gmail.com>
2025-01-08 20:57:41 -0300

    [section-24] feat: implement mongo connection and develop service

--- section-24/src/main/java/com/dziuba/resources/UserResource.java
+++ section-24/src/main/java/com/dziuba/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.dziuba.resources;
 
 import com.dziuba.domain.User;
+import com.dziuba.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,11 +12,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User user = new User("123456", "Rian", "rian@gmail.com");
-        User user2 = new User("123456", "Mateus", "mateus@gmail.com");
-        List<User> users = List.of(user, user2);
+        List<User> users = this.userService.findAll();
         return ResponseEntity.ok(users);
     }
 }
---
+++ section-24/src/main/java/com/dziuba/services/UserService.java
@@ -0,0 +1,23 @@
+package com.dziuba.services;
+
+import com.dziuba.domain.User;
+import com.dziuba.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return this.userRepository.findAll();
+    }
+}
