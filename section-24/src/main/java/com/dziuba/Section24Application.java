61831bf351ec07e4589bb8e60659f060e7522dc6
riandziuba <riandziuba@gmail.com>
2025-01-08 20:26:25 -0300

    [section-24] start: first commit

---
+++ section-24/src/main/java/com/dziuba/Section24Application.java
@@ -0,0 +1,11 @@
+package com.dziuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section24Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Section24Application.class, args);
+    }
+}
