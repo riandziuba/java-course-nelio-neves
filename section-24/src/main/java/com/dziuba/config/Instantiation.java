5603468a9da5fce1388767140b6bfc4b31ef2918
riandziuba <riandziuba@gmail.com>
2025-02-12 20:41:30 -0300

    [section-24] feat: implemented post with user as author

--- section-24/src/main/java/com/dziuba/config/Instantiation.java
+++ section-24/src/main/java/com/dziuba/config/Instantiation.java
@@ -1,31 +1,45 @@
 package com.dziuba.config;
 
+import com.dziuba.domain.Post;
 import com.dziuba.domain.User;
+import com.dziuba.repository.PostRepository;
 import com.dziuba.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
 
     @Autowired
-    public Instantiation(UserRepository userRepository) {
+    public Instantiation(UserRepository userRepository, PostRepository postRepository) {
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         this.userRepository.deleteAll();
+        this.postRepository.deleteAll();
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post postOne = new Post("Partiu viagem", null, sdf.parse("21/03/2018"), "Vou viajar para São Paulo. Abraços!", maria);
+        Post postTwo = new Post("Bom dia", null, sdf.parse("23/03/2018"), "Acordei feliz hoje!", maria);
+
         this.userRepository.saveAll((Arrays.asList(maria, alex, bob)));
+        this.postRepository.saveAll(Arrays.asList(postOne, postTwo));
     }
 }
---
+++ section-24/src/main/java/com/dziuba/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.dziuba.repository;
+
+import com.dziuba.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
